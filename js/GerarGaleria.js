a2b291a917cf6fd040a19211daff9f07af74cfbb
designergam <112179849+designergam@users.noreply.github.com>
2022-11-04 13:06:24 -0300

    Add files via upload

--- js/GerarGaleria.js
+++ js/GerarGaleria.js
@@ -1,6 +1,7 @@
+// Jogos de Computador////////////////////////////////////
 const pagina = new URLSearchParams(window.location.search);
 const paginaAtual = pagina.get('pagina');
-console.log(paginaAtual)
+
 
 
 // criar galeria de jogos
@@ -8,68 +9,39 @@ if(paginaAtual == null){
     var acriarLI = document.getElementById('ul')
     acriarLI.innerHTML = "";
     for(let i=0; i<30; i++){
-        acriarLI.innerHTML+=`
-        <li>
-            <a href="Download.html?escolhido=`+pc[i].id+`&tipo=pc">
-            <img src="`+pc[i].src+`" alt="`+pc[i].id+pc[i].tit+`" class="gallery-items">
-            </a>
-            <h2>`+pc[i].tit+`</h2>
-        </li>`;
-    }
-}
-else if(paginaAtual == 2){
-    var acriarLI = document.getElementById('ul')
-    acriarLI.innerHTML = "";
-    for(let i=31; i<61; i++){
-        acriarLI.innerHTML+=`
-        <li>
-            <a href="Download.html?escolhido=`+pc[i].id+`&tipo=pc">
-            <img src="`+pc[i].src+`" alt="`+pc[i].id+pc[i].tit+`" class="gallery-items">
-            </a>
-            <h2>`+pc[i].tit+`</h2>
-        </li>`;
+
+        if(Jogos[i].Formato == "Iso"){
+            acriarLI.innerHTML+=`
+            <li>
+                <a href="Download.html?id=`+Jogos[i].id+`">
+                <img src="`+Jogos[i].Capa+`" alt="`+Jogos[i].Nome+Jogos[i].Descricao+`" class="gallery-items">
+                </a>
+                <h2>`+Jogos[i].Nome+`</h2>
+            </li>`;
+        }
     }
 }
 
-else if(paginaAtual == 3){
-    var acriarLI = document.getElementById('ul')
-    acriarLI.innerHTML = "";
-    for(let i=62; i<92; i++){
-        acriarLI.innerHTML+=`
-        <li>
-            <a href="Download.html?escolhido=`+pc[i].id+`&tipo=pc">
-            <img src="`+pc[i].src+`" alt="`+pc[i].id+pc[i].tit+`" class="gallery-items">
-            </a>
-            <h2>`+pc[i].tit+`</h2>
-        </li>`;
-    }
-}
+// Programas ////////////////////////////////////////////////////////////////////
+else if(paginaAtual == "programas"){
 
-else if(paginaAtual == 4){
     var acriarLI = document.getElementById('ul')
     acriarLI.innerHTML = "";
-    for(let i=93; i<123; i++){
-        acriarLI.innerHTML+=`
-        <li>
-            <a href="Download.html?escolhido=`+pc[i].id+`&tipo=pc">
-            <img src="`+pc[i].src+`" alt="`+pc[i].id+pc[i].tit+`" class="gallery-items">
-            </a>
-            <h2>`+pc[i].tit+`</h2>
-        </li>`;
-    }
-}
+    for(let i=0; i<30; i++){
 
-else if(paginaAtual == 5){
-    var acriarLI = document.getElementById('ul')
-    acriarLI.innerHTML = "";
-    for(let i=124; i<154; i++){
-        acriarLI.innerHTML+=`
-        <li>
-            <a href="Download.html?escolhido=`+pc[i].id+`&tipo=pc">
-            <img src="`+pc[i].src+`" alt="`+pc[i].id+pc[i].tit+`" class="gallery-items">
-            </a>
-            <h2>`+pc[i].tit+`</h2>
-        </li>`;
+        if(Jogos[i].Formato == "programa"){
+            acriarLI.innerHTML+=`
+            <li>
+                <a href="Download.html?id=`+Jogos[i].id+`">
+                <img src="`+Jogos[i].Capa+`" alt="`+Jogos[i].Nome+Jogos[i].Descricao+`" class="gallery-items">
+                </a>
+                <h2>`+Jogos[i].Nome+`</h2>
+            </li>`;
+        }
     }
 }
+        
     
+
+
+
