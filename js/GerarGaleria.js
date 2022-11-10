315ca8f9966a87d273e8d673dc4b3e1f91a79691
designergam <112179849+designergam@users.noreply.github.com>
2022-11-09 22:57:17 -0300

    Add files via upload

--- js/GerarGaleria.js
+++ js/GerarGaleria.js
@@ -5,13 +5,64 @@ const PaginaURL = Url.get('p');
 
 
 let acriarLI = document.getElementById('ul')
-acriarLI.innerHTML = "";
-
 
 let PassarPagina = document.querySelector(".ls-pagination-filter")
 
+//Filtrar por Objetos que são do tipo "jogo"
 const jogosPC = BD.filter(item => item.Tipo == "jogo")
+
+//Filtrar por Objetos que são do tipo "programa"
 const Programas = BD.filter(item2 => item2.Tipo == "programa")
+
+
+//Ordenar Jogos em orgem alfabetica
+jogosPC.sort(function(a, b){
+    if(a.Nome < b.Nome){
+        return -1;
+    }
+    else{
+        return true;
+    }
+
+})
+
+BD.sort(function(a, b){
+    if(a.Nome < b.Nome){
+        return -1;
+    }
+    else{
+        return true;
+    }
+
+})
+
+//pesquisa
+function pesquisar(){
+
+    let pesquisa = document.getElementById("input").value
+    acriarLI.innerHTML="";
+    BD.map((jogo) => {
+        let novonome = jogo.Nome.toLowerCase()
+        let novadesc = jogo.Descricao.toLocaleLowerCase()
+        if(novonome.indexOf(pesquisa.toLowerCase()) > -1 || novadesc.indexOf(pesquisa.toLowerCase()) > -1){
+            acriarLI.innerHTML+=`
+            <li>
+                <a href="Download.html?id=`+jogo.id+`">
+                    <img src="`+jogo.Capa+`" alt="`+jogo.Nome+`" class="gallery-items">
+                </a>
+                <a href="Download.html?id=`+jogo.id+`">
+                    <h2>`+jogo.Nome+`</h2>
+                </a>
+            </li>`;
+        }}
+        )
+
+        PassarPagina.style.display = "none"
+   
+}
+
+
+
 
 // Tipo: Jogos - Pagina: 1
 if(TipoURL == null & PaginaURL == null || TipoURL == "jogo" & PaginaURL == 1){
@@ -35,6 +86,7 @@ if(TipoURL == null & PaginaURL == null || TipoURL == "jogo" & PaginaURL == 1){
     <p id="textPaginas">Paginas >>></p>
     <a href="?t=jogo&p=1"><li id="pg1">1</li></a>
     <a href="?t=jogo&p=2"><li id="pg2">2</li></a>
+    <a href="?t=jogo&p=3"><li id="pg3">3</li></a>
           
     </ul>
     `
@@ -42,7 +94,7 @@ if(TipoURL == null & PaginaURL == null || TipoURL == "jogo" & PaginaURL == 1){
 // Tipo: Jogos - Pagina: 2
 else if(TipoURL == "jogo" & PaginaURL == 2){
    
-    for(let i=18; i<24; i++){
+    for(let i=18; i<36; i++){
 
         acriarLI.innerHTML+=`
         <li>
@@ -61,11 +113,43 @@ else if(TipoURL == "jogo" & PaginaURL == 2){
           <p id="textPaginas">Paginas >>></p>
           <a href="?t=jogo&p=1"><li id="pg1">1</li></a>
           <a href="?t=jogo&p=2"><li id="pg2">2</li></a>
+          <a href="?t=jogo&p=3"><li id="pg3">3</li></a>
           
     </ul>
     `
    
 }
+// Tipo: Jogos - Pagina: 3
+else if(TipoURL == "jogo" & PaginaURL == 3){
+   
+    for(let i=36; i<54; i++){
+
+        acriarLI.innerHTML+=`
+        <li>
+            <a href="Download.html?id=`+jogosPC[i].id+`">
+                <img src="`+jogosPC[i].Capa+`" alt="`+jogosPC[i].Nome+`" class="gallery-items">
+            </a>
+            <a href="Download.html?id=`+jogosPC[i].id+`">
+                <h2>`+jogosPC[i].Nome+`</h2>
+            </a>
+        </li>`;
+    }
+
+    PassarPagina.innerHTML =`
+
+    <ul class="ls-pagination">
+          <p id="textPaginas">Paginas >>></p>
+          <a href="?t=jogo&p=1"><li id="pg1">1</li></a>
+          <a href="?t=jogo&p=2"><li id="pg2">2</li></a>
+          <a href="?t=jogo&p=3"><li id="pg3">3</li></a>
+          
+    </ul>
+    `
+   
+}
+
+
+
 // Tipo: Programas - Pagina: 1
 else if(TipoURL == "programa" & PaginaURL == 1){
     for(let i=0; i<1; i++){
@@ -95,15 +179,14 @@ else if(TipoURL == "programa" & PaginaURL == 1){
 
 //Mostar Pagina Selecionada
 if(PaginaURL == 1){
-    console.log("k")
+   
 
     let itempagina = document.getElementById("pg1")
     itempagina.style.background = "#263241"
     itempagina.style.color = "white"
 }
 else if(PaginaURL == 2){
-    console.log("k")
-
+   
     let itempagina = document.getElementById("pg2")
     itempagina.style.background = "#263241"
     itempagina.style.color = "white"
