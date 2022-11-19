dc7cef00cdb736223fde845dbfeb4992b8aa247b
designergam <112179849+designergam@users.noreply.github.com>
2022-11-19 14:14:46 -0300

    Add files via upload

--- js/Download.js
+++ js/Download.js
@@ -1,42 +1,47 @@
-const ID0 = window.location.href; 
+//Pegar parametro da URL
+    const ID0 = window.location.href; 
     const ID1 = new URL(ID0);
     let ID = ID1.searchParams.get("id");
+
+    let conteudo = [];
+        conteudo = BD.filter(function(item){
+        return item.id == ID;
+    })
+
+    let Nome = (conteudo[0].Nome);
+    let Capa = (conteudo[0].Capa)
+    let Descricao = (conteudo[0].Descricao)
+    let Trailer = (conteudo[0].Trailer)
+    let Imagem= (conteudo[0].Imagem)
+    let Gif = (conteudo[0].Gif)
+    let cpu_A = (conteudo[0].Requisitos);
     
-        let conteudo = [];
-
-         conteudo = BD.filter(function(item){
-            return item.id == ID;
-        })
-
-        let Nome = (conteudo[0].Nome);
-        let Capa = (conteudo[0].Capa)
-        let Descricao = (conteudo[0].Descricao)
-        let Trailer = (conteudo[0].Trailer)
+    function goBack() {
+        window.history.back()
+    }
         
-        let Imagem= (conteudo[0].Imagem)
-        let Gif = (conteudo[0].Gif)
-
-//Requisitos Minimos
-        let cpu_A = (conteudo[0].Requisitos);
-        
-
-        function goBack() {
-            window.history.back()
-        }
 //Se não tiver descrição
-        if(conteudo[0].Descricao == `undefined`){
-            Descricao = `A descrição deste jogo não está disponível no momento porem fique à vontade para baixar o jogo normalmente. Em breve novas atualizações na página obrigado!`
-        }
+    if(conteudo[0].Descricao == `undefined`){
+        Descricao = `A descrição deste jogo não está disponível no momento porem fique à vontade para baixar o jogo normalmente. Em breve novas atualizações na página obrigado!`
+    }
+
 //Escrever a pagina de download com array   
-    let src_B =  window.document.getElementById("img_capa").src = Capa
-    let previa_B =  window.document.getElementById("previa").innerText = Descricao
-    let tit_B =  window.document.getElementById("tit").innerText = Nome 
-    let img_gameplay1_B =  window.document.getElementById("img_gameplay1").src = Imagem
-    let img_gameplay2_B =  window.document.getElementById("img_gameplay2").src = Gif
-    let cpu_B =  window.document.getElementById("Requisitos").innerText = cpu_A 
-    
-    let trailer_B =  window.document.getElementById("trailer").src = `https://www.youtube.com/embed/`+Trailer+``
-    let baixarbtn = document.getElementById("btnDownloadArea").innerHTML=`<a href="Redirect.html?id=`+ID+`" id="download">DOWNLOAD</a>`
+    let src_B =  window.document.getElementById("img_capa")
+     src_B.src = Capa
+    let previa_B =  window.document.getElementById("previa")
+        previa_B.innerText = Descricao
+    let tit_B =  window.document.getElementById("tit")
+        tit_B.innerText = Nome 
+    let img_gameplay1_B =  window.document.getElementById("img_gameplay1")
+        img_gameplay1_B.src = Imagem
+    let img_gameplay2_B =  window.document.getElementById("img_gameplay2")
+        img_gameplay2_B.src = Gif
+    let cpu_B =  window.document.getElementById("Requisitos") 
+        cpu_B.innerText = cpu_A 
+    let trailer_B =  window.document.getElementById("trailer")
+        trailer_B.src = `https://www.youtube.com/embed/`+Trailer+``
+    let baixarbtn = document.getElementById("btnDownloadArea")
+        baixarbtn.innerHTML=`<a href="Redirect.html?id=`+ID+`" id="download">DOWNLOAD</a>`
 
 //Trailer, fecha o container se nao tiver nada
     if(conteudo[0].Trailer == "undefined"){
@@ -50,7 +55,7 @@ const ID0 = window.location.href;
         ReT.style.display = "none"
     }
     
-//Imagem e Gid, fecha o container se nao tiver nada
+//Imagem e Gif, fecha o container se nao tiver nada
     if(conteudo[0].Imagem == "undefined"){
         let IeG = document.getElementById("container")
         let IdJ = document.getElementById("TEXTO_H2")
@@ -64,19 +69,24 @@ const ID0 = window.location.href;
 
 //Meta Tags e SEO
     let description = document.getElementById("description")
-    description.content="download jogo x utorrent, baixar jogo x em capitaotorrent.com, baixar o jogo x via torrent "
+        description.content="download jogo x utorrent, baixar jogo x em capitaotorrent.com, baixar o jogo x via torrent "
 
     let keywords = document.getElementById("keywords")
-    keywords.content=`download `+conteudo[0].Nome+`, baixar `+conteudo[0].Nome+`, `+conteudo[0].Nome+` torrent, `+conteudo[0].Nome+` utorrent, `+conteudo[0].Nome+` gratis, download gratis `+conteudo[0].Nome+`, `+conteudo[0].Nome+`, torrent jogos, utorrent download, games torrent, download game`
+        keywords.content=`download `+conteudo[0].Nome+`, baixar `+conteudo[0].Nome+`, `+conteudo[0].Nome+` torrent, `+conteudo[0].Nome+` utorrent, `+conteudo[0].Nome+` gratis, download gratis `+conteudo[0].Nome+`, `+conteudo[0].Nome+`, torrent jogos, utorrent download, games torrent, download game`
 
-    let ogUrl = document.getElementById("ogUrl").content=``+window.location.href+``
-    let ogTitle = document.getElementById("ogTitle").content=``+conteudo[0].Nome+``
-    let ogImage = document.getElementById("ogImage").content=``+conteudo[0].Capa+``
-    let ogDescription = document.getElementById("ogDescription").content=` Download Grátis de via Torrent`
+    let ogUrl = document.getElementById("ogUrl")
 
+    let ogTitle = document.getElementById("ogTitle")
+        ogTitle.content=``+conteudo[0].Nome+``
+
+    let ogImage = document.getElementById("ogImage")
+        ogImage.content=``+conteudo[0].Capa+``
+
+    let ogDescription = document.getElementById("ogDescription")
+        ogDescription.content=` Download Grátis de via Torrent`
 
     let fundo = document.getElementById("container2")
-    fundo.style.backgroundImage=`url(`+conteudo[0].Imagem+`)`
+        fundo.style.backgroundImage=`url(`+conteudo[0].Imagem+`)`
 
 //-------------------------------RECOMENDADOS---------------------------------
 //Filtrar por Objetos de mesmo genero e do tipo "jogo"
@@ -85,7 +95,7 @@ const ID0 = window.location.href;
 //Remove Item Atual dos recomendados
     const Genero = GeneroA.filter((item) => item.Nome !== conteudo[0].Nome);
     
-        let recomendados = document.getElementById("RecomendadosLi")
+    let recomendados = document.getElementById("RecomendadosLi")
 
 //Abrir container3 se tiver Genero     
 if(conteudo[0].Genero){
@@ -109,7 +119,8 @@ if(conteudo[0].Genero){
 }
 //Fechar container3 se não tiver Genero
 else{
-    let close = document.getElementById("container3").style.display="none"
+    let close = document.getElementById("container3")
+    close.style.display="none"
 }
 
 
