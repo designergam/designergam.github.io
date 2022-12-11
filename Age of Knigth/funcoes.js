0f5739d561b213957058d720f4d18af6f80282b8
designergam <112179849+designergam@users.noreply.github.com>
2022-12-10 22:49:06 -0300

    Add files via upload

--- Age of Knigth/funcoes.js
+++ Age of Knigth/funcoes.js
@@ -1,13 +1,184 @@
+//atributos de batalha do player
+    atributos_player.innerHTML =`
+    <p> HP: ${localStorage.HP_player}</p>
+    <p> DEF: ${localStorage.DEF_player}</p>
+    <p> ATK: ${localStorage.ATK_player}</p>
+    `
 
-mostrarlevel()
+    mostrarlevel()
+
+function jogar(){
+    localStorage.play = false
+    //ocutar botão de play
+        play.style.display="none"
+    
+    //INIMIGO ALEATORIO
+        localStorage.inimigo = Math.floor(4* Math.random()) 
+
+    //trazer inimigo pro cenario
+        inimigo.style.display="block"
+
+        setTimeout(function(){
+            inimigo.style.transition="600ms"
+            inimigo.style.right="15%"
+        },30)
+    
+
+    //ataque do jogador
+    golpe_player = setInterval(function(){
+
+        //evitar dano negativo
+            dano_player = (localStorage.ATK_player - localStorage.DEF_inimigo)
+            if(dano_player<=0){dano_player = 1}
+
+        //calcular dano no inimigo
+            localStorage.HP_inimigo = localStorage.HP_inimigo - dano_player
+
+        //atacar
+            setTimeout(function(){
+                trocar_img_player_golpe()
+                som_golpe_espada()
+                player.style.left="35%"
+            },100)
+            
+        //recuar
+            setTimeout(function(){
+                player_parado()
+                player.style.left="15%"
+                trocar_cor_inimigo()
+            },800)
+
+        //mostrar atributos inimigo
+            atributos_inimigo.innerHTML =`
+            <p> HP: ${localStorage.HP_inimigo}</p>
+            <p> DEF: ${localStorage.DEF_inimigo}</p>
+            <p> ATK: ${localStorage.ATK_inimigo}</p>
+            ` 
+        //se ganhar
+            if(localStorage.HP_inimigo <= 0){
+                //parar luta
+                    clearInterval(golpe_inimigo)
+                    clearInterval(golpe_player)
+
+                //tocar som de vitoria
+                    setTimeout(function(){
+                        vitoria_som()
+                    },800)
+
+                //mostar cartas
+                    setTimeout(function(){
+                        mostrar_cartas()
+                    },800)
+
+                //mensagem de vitoria
+                    mensagem_ganhou()
+
+                //atribuir vitoria
+                    vitoria ()
+
+                //mostrar level
+                    mostrarlevel()
+
+                //tornar inimigo mais forte
+                    inimigo_mais_forte()
+
+                //voltar a vida anterior e levelup
+                    reset_vida_e_levelUp()
+
+                //deitar inimigo e esconder
+                    inimigo_cair_para_traz()
+
+                //esconder inimigo morto
+                    atributos_inimigo.style.display="none"
+                    
+            }
+
+    }, 2000)
+
+    //ataque do inimigo
+    golpe_inimigo = setInterval(function(){
+
+        //evitar dano negativo
+            dano_inimigo = (localStorage.ATK_inimigo - localStorage.DEF_player)
+            if(dano_inimigo<=0){dano_inimigo = 1}
+        
+        //calcular dano no jogador
+            localStorage.HP_player = (localStorage.HP_player - dano_inimigo)
+
+        //atacar
+            setTimeout(function(){
+                som_golpe_espada()
+                inimigo.style.right="35%"
+            },100)
+        
+        //recuar
+            setTimeout(function(){
+                
+                inimigo.style.right="15%"
+                trocar_cor_player()
+            },800)
+
+        //mostrar atributos do inimigo  
+            atributos_player.innerHTML =`
+            <p> HP: ${localStorage.HP_player}</p>
+            <p> DEF: ${localStorage.DEF_player}</p>
+            <p> ATK: ${localStorage.ATK_player}</p>
+            `
+        //se perder
+            if(localStorage.HP_player <= 0){
+                //parar luta
+                    clearInterval(golpe_player)
+                    clearInterval(golpe_inimigo)
+                //mostrar mensagem de derrota
+                    mensagem_derrota()
+
+                //resetar game over
+                    setTimeout(function(){
+                        resetar()
+                    }, 3000)
+         
+            }
+
+    }, 2000)
+  
+}
+
+
+function mostrar_cartas(){
+    //esconder mensagem de vitoria
+        mensagem.innerText=""
+    
+    //mostrar cartas
+        cartas.style.display="block"
+        card1.style.display="block"
+        card2.style.display="block"
+        card3.style.display="block"
+
+    //movimentar ao centro
+        setInterval(function(){
+            card1.style.top="50%"
+            card2.style.top="50%"
+            card3.style.top="50%"
+        }, 500)
+    //espalhar na tela
+        setInterval(function(){
+            card2.style.left="80%"
+            card3.style.left="20%"
+        }, 1000)
+
+}
+function fechar_cards(){
+    
+    //esconder cartas
+    document.querySelector("#container_cards").style.display="none"
+
+    localStorage.play = true
+    window.location.reload() 
+ 
+}
 function mostrarlevel(){
-    document.getElementById("level").innerText += `${localStorage.vitorias}`
+    document.getElementById("level").innerText = `LEVEL:  ${localStorage.vitorias}`
 }
-
-function recarregar(){
-    window.location.reload()
-}
-
 function resetar(){
     localStorage.clear();
     window.location.reload()
@@ -22,12 +193,10 @@ async function reset_vida_e_levelUp(){
     localStorage.ATK_player = 6 + parseInt(localStorage.vitorias) 
     localStorage.DEF_player = 5 + parseInt(localStorage.vitorias) 
 }
-
-async function vitoria (){
+async function vitoria(){
     localStorage.vitorias ++
-    localStorage.boleano = true
+   
 }
-
 async function mensagem_ganhou(){
     mensagem.innerHTML=`
     <h2>VICTORY!</h2> <br>
@@ -35,18 +204,17 @@ async function mensagem_ganhou(){
     `
 }
 async function trocar_cor_inimigo(){
-    let inimigo_cor = document.getElementById("inimigo_img")
     inimigo_cor.style.transition="200ms"
     inimigo_cor.style.filter="opacity(0.6) drop-shadow(0 0 0 red)"
     setTimeout(function(){
-        inimigo_cor.style.transition="400ms"
+        inimigo_cor.style.transition="300ms"
         inimigo_cor.style.filter="none"
     },180)
 
 
 }
 async function trocar_cor_player(){
-    let player_cor = document.getElementById("player_img")
+    
     player_cor.style.transition="200ms"
     player_cor.style.filter="opacity(0.6) drop-shadow(0 0 0 red)"
     setTimeout(function(){
@@ -56,166 +224,48 @@ async function trocar_cor_player(){
 
 
 }
+function inimigo_cair_para_traz(){
+    //fazer inimigo cair pra traz
+    inimigo.style.transform="translate(20%, 40%) rotate(90deg) scaleX(-1)"
 
-function inimigo_morre(){
-    
-    document.getElementById("inimigo_img").style.transform="translate(20%, 40%) rotate(90deg)"
-
+    //inimigo volta a posição inicial
     setTimeout(function(){
-        document.getElementById("inimigo_img").style.display="none"
-    },1000)
+        inimigo.style.display="none"
+        inimigo.style.transform="translate(0%, 0%) rotate(0deg) scaleX(-1)"
+    },800)
 
     
-        
-   
 }
-
 function mensagem_derrota(){
     mensagem.innerText="YOU LOST!"
 }
-
-
-//movimentação player================================================================
-function player_parado(){
-    player.src="https://cdn.domestika.org/c_limit,dpr_1.0,f_auto,q_auto,w_820/v1586415165/content-items/004/129/278/personaje-parada-original.gif?1586415165"
+async function player_parado(){
+    player.style.background="url(https://cdn.domestika.org/c_limit,dpr_1.0,f_auto,q_auto,w_820/v1586415165/content-items/004/129/278/personaje-parada-original.gif?1586415165"
+    player.style.backgroundPositionY="7px"
+    player.style.backgroundPositionX="center"
+    player.style.backgroundRepeat="no-repeat"
+    player.style.backgroundSize="130%"
 }  
-
-function player_golpe(){
-    player.src="https://cdn.domestika.org/c_limit,dpr_1.0,f_auto,q_auto,w_820/v1586415892/content-items/004/129/334/personaje_Ataque-original.gif?1586415892"
+async function trocar_img_player_golpe(){
+    player.style.background="url(https://cdn.domestika.org/c_limit,dpr_1.0,f_auto,q_auto,w_820/v1586415892/content-items/004/129/334/personaje_Ataque-original.gif?1586415892"
+    player.style.backgroundPositionY="7px"
+    player.style.backgroundPositionX="center"
+    player.style.backgroundRepeat="no-repeat"
+    player.style.backgroundSize="130%"
+    
+    
 }  
-
-
-if(localStorage.vitorias == 0){
-    localStorage.boleano = "true"
-    
+function som_golpe_espada(){
+    som.src="audios/Golpe com espada.mp3"
+    audio.volume = 0.3;
+    audio.play()
 }
-
-if(localStorage.boleano == "false"){
-    play()
+function vitoria_som(){
+    som.src="audios/vitoria.wav"
+    audio.volume = 0.09;
+    audio.play()
 }
-
-
-function play(){
-   //INIMIGO ALEATORIO
-    localStorage.inimigo = Math.floor(4* Math.random())
-    
-
-    //movimentar inimigo
-    document.getElementById("inimigo_img").style.display="block"
-    setTimeout(function(){
-        document.getElementById("inimigo_img").style.transition="1000ms"
-    
-        document.getElementById("inimigo_img").style.right="-20px"
-    },30)
-    
-    //ocutarplayer
-    localStorage.play="off"
-    setTimeout(function(){
-        document.getElementById("play").style.display="none"
-    },500)
-
-    
-    
-//player==================================
-    let golpe_player = setInterval(function(){
-
-        //evitar dano negativo
-        let dano_player = (localStorage.ATK_player - localStorage.DEF_inimigo)
-        if(dano_player<=0){dano_player = 0}
-
-        localStorage.HP_inimigo = localStorage.HP_inimigo - dano_player
-
-        //movimentação
-        player_golpe()
-        golpe_espada()
-        player.style.left="280px"
-
-        setTimeout(function(){
-            player_parado()
-            player.style.left="0px"
-            trocar_cor_inimigo()
-        },700)
-        
-        //se ganhar=====================
-        if(localStorage.HP_inimigo <= 0){
-            //parar batalha
-            clearInterval(golpe_inimigo)
-            clearInterval(golpe_player)
-
-            setTimeout(function(){
-                vitoria_som()
-            },800)
-
-            
-            
-            mensagem_ganhou()
-            vitoria ()
-    
-            inimigo_mais_forte()
-            reset_vida_e_levelUp()
-            inimigo_morre()
-            setTimeout(function(){
-                recarregar()
-            },3000)
-
-            atributos_inimigo.style.display="none"
-        }
-
-        atributos_inimigo.innerHTML =`
-        <p> HP: ${localStorage.HP_inimigo}</p>
-        <p> DEF: ${localStorage.DEF_inimigo}</p>
-        <p> ATK: ${localStorage.ATK_inimigo}</p>
-        `
-        
-         
-    }, 2000)
-
-
-
-
-
-
-
-//inimigo=================================
-    let golpe_inimigo = setInterval(function(){
-
-        //evitar dano negativo
-        let dano_inimigo = (localStorage.ATK_inimigo - localStorage.DEF_player)
-        if(dano_inimigo<=0){dano_inimigo = 0}
-        
-        localStorage.HP_player = (localStorage.HP_player - dano_inimigo)
-
-        //movimentação
-        
-        golpe_espada()
-        inimigo.style.right="400px"
-
-        setTimeout(function(){
-            
-            inimigo.style.right="100px"
-            trocar_cor_player()
-        },700)
-
-        //se perder===
-        if(localStorage.HP_player <= 0){
-            //parar batalha
-            clearInterval(golpe_player)
-            clearInterval(golpe_inimigo)
-
-            mensagem_derrota()
-    
-            setTimeout(function(){
-                resetar()
-            }, 3000)
-             
-        }
-
-        atributos_player.innerHTML =`
-            <p> HP: ${localStorage.HP_player}</p>
-            <p> DEF: ${localStorage.DEF_player}</p>
-            <p> ATK: ${localStorage.ATK_player}</p>
-            `
-
-    }, 3000)
-  
+function musica_fundo(){
+    audio.volume = 0.1;
+    audio.play()
 }
--- Age of Knigth/variaveis.js
+++ Age of Knigth/variaveis.js
@@ -1,16 +1,48 @@
-let player = document.getElementById("player_img")
-let inimigo = document.getElementById("inimigo_img")
+//cartas
+    let card1 = document.getElementById("card1")
+    let card2 = document.getElementById("card2")
+    let card3 = document.getElementById("card3")
+
+//texto de exibição da carta
+    let h1_atk = document.getElementById("h1_atk")
+    let h1_def = document.getElementById("h1_def")
+    let h1_hp = document.getElementById("h1_hp")
+
+//botão de play
+    let play = document.getElementById("play")
 
 //mensagem
-let mensagem = document.getElementById("mensagem")
+    let mensagem = document.getElementById("mensagem")
 
-//atributos de batalha do player
-let atributos_player = document.getElementById("atributos_player")
-atributos_player.innerHTML =`
-<p> HP: ${localStorage.HP_player}</p>
-<p> DEF: ${localStorage.DEF_player}</p>
-<p> ATK: ${localStorage.ATK_player}</p>
-`
-    
- 
+//cenario
+    let cenario = document.getElementById("cenario")
+
+//inimigo
+    let inimigo = document.getElementById("inimigo_img")
+
+//jogador
+    let player = document.getElementById("player_img")
+
+//atributos do jogador
+    let atributos_player = document.getElementById("atributos_player")
+
+//cartas
+    let cartas = document.querySelector("#container_cards")
+
+//golpe
+    let golpe_player=""
+    let golpe_inimigo=""
+
+//evitar dano negativo
+    let dano_player=""
+    let dano_inimigo=""
+
+//efeitos sonoros
+    let som = document.getElementById("audio")
+
+//musica
+    let audio = document.getElementById("audio")
+
+
+
     
